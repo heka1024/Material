a5cff4b13d6268638c64aad529cfbfccb94d2f96
heka1024 <heka1024@gmail.com>
2018-12-19 16:01:24 +0900

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "lib/Material.h"
+#include "lib/Matrix.h"
+#include "lib/Vector.h"
+std::string inputName = "../input/input.txt";
+
+int main() {
+    Structure s;
+    std::ifstream ifs(inputName);
+    ifs >> s; ifs.close();
+    s.moment3();
+    std::vector<Mesh> meshes;
+    s.calcGraph(meshes);
+    s.ls();
+    std::ofstream ofs("../data.txt");
+    for(auto x : meshes) {
+        ofs << x;
+    }
+    ofs.close();
+
+
+    return 0;
+}
